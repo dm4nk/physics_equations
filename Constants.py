8541e3a957420762215830095b97a88445b374d4
dm4nk <dim.xx2011@yandex.ru>
2022-04-06 09:41:51 +0400

    recalculated integral with psi

--- Constants.py
+++ Constants.py
@@ -2,8 +2,8 @@ import enum
 
 
 class Constants(enum.Enum):
-    D = 0.06
+    D = 3
     H = 0
     U_c = 0
     L = 12
-    T = 150
+    T = 400
--- func.py
+++ func.py
@@ -1,11 +1,9 @@
 import math
-from scipy.integrate import quad
 from Constants import Constants
 
 p = Constants.H.value * Constants.L.value
 L = Constants.L.value
 D = Constants.D.value
-U_c = Constants.U_c.value
 
 
 def func(mu):
@@ -21,10 +19,9 @@ def u(x, t, mu_array):
 
     for mu_k in mu_array:
         _sum += \
-            (mu_k ** 2 + p ** 2) / \
-            (p * (p + 1) + mu_k ** 2) * \
+            2 / mu_k * \
             math.e ** (-D * mu_k ** 2 * t / L ** 2) * \
             math.cos(mu_k * x / L) * \
-            quad(lambda _x: (psi(_x) + U_c) * math.cos(mu_k * _x / L), 0, L)[0]
+            (math.sin(3 * mu_k / 4) - math.sin(mu_k / 4))
 
-    return 2 / L * _sum + U_c
+    return _sum
