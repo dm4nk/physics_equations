7b5432e22f56ab958ced6bd1354850f75d9423d3
dm4nk <dim.xx2011@yandex.ru>
2022-05-29 20:11:08 +0400

    refactored web

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def gm(d=0.06, l=12, t=150, e=0.01):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
